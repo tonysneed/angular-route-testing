f839fdd03aea440c59d16d07f2b59198bdc35b2b
Anthony Sneed <tony@tonysneed.com>
2017-06-27 06:34:36 -0500

    Add dashboard component, feature module with routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FeatureModule } from 'app/feature/feature.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FeatureModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/feature/feature-routing.module.ts
+++ src/app/feature/feature-routing.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { FeatureComponent } from 'app/feature/feature.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
-  { path: 'dashboard', component: DashboardComponent },
   { path: 'feature', component: FeatureComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class FeatureRoutingModule { }
